4120d1bc5aaa323937c5289cf2686c1ae182323c
Yusee-ly-ya <Yangli1011@outlook.com>
2020-09-30 10:37:14 +0800

    初始化提交

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,15 @@
+from .base import *
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "typeidea",
+        'USER': "root",
+        'PASSWORD': 123456,
+        'HOST': '127.0.0.1',
+        'PORT': 3306
+    }
+}
